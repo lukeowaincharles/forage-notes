c8ffd35978d025ed9eb0c80cfe2152ebbe5f6a4c
unknown <lukeowaincharles@gmail.com>
2018-02-22 19:16:25 +0000

    initial commit

---
+++ public_html/static/js/master.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+//  $('.hide-button').on('click', () => {
+//    $('.form-control, .second').fadeOut(800);
+//    $('.thankyou__message').fadeIn(1000);
+//  });
+  
+  
+  $('.hide-button').on('click', () => {
+      $('body').addClass('codingIsFun');
+  });
+  
+  
+  
+  
+  
+  
+  
+});
+
+$(document).ready(function (){
+   $('.hide-button').click(function (){
+      $('body').addClass('codingIsFun');
+  }); 
+});
+ 
